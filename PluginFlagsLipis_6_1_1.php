42b0306fb87bdc38b01aa7a8fe164ed2df0f1b1a
Claes Östlund <claes@stenbergit.se>
2022-04-07 19:10:11 +0200

    1.1.0

--- PluginFlagsLipis_6_1_1.php
+++ PluginFlagsLipis_6_1_1.php
@@ -4,4 +4,24 @@ class PluginFlagsLipis_6_1_1{
     $element = new PluginWfYml(__DIR__.'/element/'.__FUNCTION__.'.yml');
     wfDocument::renderElement($element);
   }
+  public function getFlagElement($name){
+    /**
+     * Rewrite $name if not corresponds to this plugin.
+     */
+    if(wfGlobals::get("settings/i18n/lable/$name")){
+      if($name=='sv' && wfGlobals::get("settings/i18n/lable/$name")=='Swedish'){
+        $name = 'se';
+      }elseif($name=='en' && wfGlobals::get("settings/i18n/lable/$name")=='English'){
+        $name = 'gb';
+      }
+    }
+    /**
+     * Element.
+     */
+    $flag = wfDocument::createHtmlElement('span', null, array('class' => "fi fi-".$name));
+    /**
+     * 
+     */
+    return $flag;
+  }
 }
